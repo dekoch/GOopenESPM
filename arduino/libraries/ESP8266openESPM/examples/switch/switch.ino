a0e420acaccc8d731da73b1957b7f85bcd9b772f
mario <mario@xps13.workgroup>
2018-04-08 20:12:56 +0200

    +arduino library

---
+++ arduino/libraries/ESP8266openESPM/examples/switch/switch.ino
@@ -0,0 +1,83 @@
+
+// control a digital output with your openESPM server
+// https://github.com/dekoch/GOopenESPM
+
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <openESPM.h>
+
+// WiFi settings
+char* ssid      = "";
+char* password  = "";
+// openESPM server settings
+char* host      = "myhost.com";     // your domain
+char* path      = "/request/";     // your folder
+char* ID        = "1234567890123";  // device id
+char* key       = "1234567890123";  // private key
+const int pin   = 2;                // output pin
+
+#define SLEEPTIME 60  //default sleep time in seconds
+#define ATTEMPTS 50
+
+
+openESPM espm;
+
+
+void setup()
+{
+  pinMode(pin, OUTPUT);
+  pinMode(pin, HIGH);
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("");
+  Serial.println("");
+
+  espm.setupWiFi(ssid, password, ATTEMPTS);
+  espm.setupESPM(host, path, ID, key);
+  espm.hello("switch", "1.0");
+}
+
+
+void loop()
+{
+  String response = espm.ESPMRequest("");
+
+  if (response != "")
+  {
+    // Parse JSON
+    int size = response.length() + 1;
+    char json[size];
+    response.toCharArray(json, size);
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& json_parsed = jsonBuffer.parseObject(json);
+    if (!json_parsed.success())
+    {
+      Serial.println("parseObject() failed");
+    }
+    else
+    {
+      // Make the decision to turn on or off the LED
+      if (strcmp(json_parsed["Switch"], "on") == 0)
+      {
+        digitalWrite(pin, LOW);
+        Serial.println("LED ON");
+      }
+      else
+      {
+        digitalWrite(pin, HIGH);
+        Serial.println("led off");
+      }
+    }
+  }
+
+  SleepSeconds(SLEEPTIME);
+}
+
+void SleepSeconds(int seconds)
+{
+  Serial.print("sleep ");
+  Serial.print(seconds);
+  Serial.println(" seconds");
+
+  delay(seconds * 1000);
+}
